850afb18807b3a3af01191dde4b74a183aed6b61
zwf <992684658@qq.com>
2020-06-02 22:41:55 +0800

    给number传入新增加的三个props，并硬核的设置颜色

--- src/Game.js
+++ src/Game.js
@@ -2,16 +2,41 @@ import React from "react";
 
 import _ from "lodash";
 require("./App.css");
-console.log(_.range(1, 10));
+//为可能出现的每种情况设置参数
+const colors = {
+  used: "lightgreen", //已使用过的数字,后面不能再选
+  wrong: "lightcoral", //选择错误
+  selected: "deepskyblue", //已选择的数字
+};
 //提取number元素
 class Number extends React.PureComponent {
-  //这是一个空的点击事件
+  style() {
+    if (this.props.isUsed) {
+      return {
+        backgroundColor: colors.used, //green
+      };
+    }
+    //
+    if (this.props.isSelected) {
+      return {
+        backgroundColor: this.props.selectionIsWrong
+          ? colors.wrong //red
+          : colors.selected, //blue
+      };
+    }
+    return {};
+  }
 
+  //这是一个空的点击事件
   clickHandler = () => console.log("Click on " + this.props.number);
 
   render() {
     return (
-      <button className="number" onClick={this.clickHandler}>
+      <button
+        className="number"
+        style={this.style()}
+        onClick={this.clickHandler}
+      >
         {/* 访问传入的props */}
         {this.props.number}
       </button>
@@ -22,7 +47,14 @@ class Game extends React.Component {
   //生成随机的星星数
   state = {
     stars: 1 + Math.floor(9 * Math.random()), //Math.random 本身只生成0-1的小数
+    //已经原则过的数字
+    selectedNumbers: [2, 4],
+    //已经使用过的数字
+    usedNumbers: [7, 8],
   };
+
+  //当已挑选的数字大于星星数量，返回wrong否则返回true
+  selectionIsWrong = _.sum(this.state.selectedNumbers) > this.state.stars;
   render() {
     return (
       <div className="game">
@@ -36,10 +68,23 @@ class Game extends React.Component {
 
           {/* 使用lodash的内置方法生成一个1-9的数组 */}
           <div className="play-numbers">
-            {/* 设置一个props */}
-            {_.range(1, 10).map((number) => (
-              <Number key={number} number={number} />
-            ))}
+            {/* 设置number，isUsed，isSelected，selectionIsWrong state */}
+            {_.range(1, 10).map((number) => {
+              //由于state中的usedNumbers的数字已经强制设置好，所以state isUsed只有2和4才有
+              const isUsed = this.state.usedNumbers.indexOf(number) >= 0;
+              //同理 state isSelected 参数只有7和8才有
+              const isSelected =
+                this.state.selectedNumbers.indexOf(number) >= 0;
+              return (
+                <Number
+                  key={number}
+                  number={number}
+                  isUsed={isUsed}
+                  isSelected={isSelected}
+                  selectionIsWrong={this.selectionIsWrong}
+                />
+              );
+            })}
           </div>
         </div>
       </div>
