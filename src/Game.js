15d5787098046457a182c35eb81147388dad5896
zwf <992684658@qq.com>
2020-06-02 21:46:42 +0800

    动态的设置星星的数量

--- src/Game.js
+++ src/Game.js
@@ -1,34 +1,31 @@
 import React from "react";
 import _ from "lodash";
 require("./App.css");
+console.log(_.range(1, 10));
 
 class Game extends React.Component {
+  //生成随机的星星数
+  state = {
+    stars: 1 + Math.floor(9 * Math.random()), //Math.random 本身只生成0-1的小数
+  };
   render() {
     return (
       <div className="game">
         <div className="help">选择一个或多个数字。使其加起来等于星星的数量</div>
         <div className="body">
           <div className="stars">
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
-            <div className="star" />
+            {_.range(this.state.stars).map((starIndex) => (
+              <div key={starIndex} className="star" />
+            ))}
           </div>
+
+          {/* 使用lodash的内置方法生成一个1-9的数组 */}
           <div className="play-numbers">
-            <button className="number">1</button>
-            <button className="number">2</button>
-            <button className="number">3</button>
-            <button className="number">4</button>
-            <button className="number">5</button>
-            <button className="number">6</button>
-            <button className="number">7</button>
-            <button className="number">8</button>
-            <button className="number">9</button>
+            {_.range(1, 10).map((number) => (
+              <button key={number} className="number">
+                {number}
+              </button>
+            ))}
           </div>
         </div>
       </div>
